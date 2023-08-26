2d1a07e5a85bf691073a741a553ff63fe5a5fb25
MinhDuc <ogdp.vn@gmail.com>
2023-08-26 14:40:23 +0700

    noisse

--- src/app.js
+++ src/app.js
@@ -78,14 +78,37 @@
 // // // module.exports = app;
 // // export const viteNodeApp = app;
 
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
+// import express from "express";
+// import cors from "cors";
+// import morgan from "morgan";
+// const app = express();
+
+// app.use(morgan("tiny"));
+// app.use(cors());
+// app.use("/", function (req, res) {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Hello World!");
+// });
+// app.use("**", (req, res, next) => {
+//   return res.status(200).json({
+//     message: "API Not Found",
+//   });
+// });
+
+// app.listen(8080, (req, res) => {
+//   console.log("Listening on port 8080");
+// });
+
+// export const viteNodeApp = app;
+
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
-app.use("/", function (req, res) {
+app.use("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello World!");
 });
@@ -99,4 +122,4 @@ app.listen(8080, (req, res) => {
   console.log("Listening on port 8080");
 });
 
-export const viteNodeApp = app;
+module.exports = app;
